988b28f400e2abea73f0b5bae3963874fc531d6c
Logan Roberts <logan@lcroberts.dev>
2025-02-22 10:20:02 -0500

    Switch to conform autoformat being an add list

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,6 +13,16 @@ return {
   },
   opts = {
     notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Enable "format_on_save lsp_fallback" for languages
+      local enable_filetypes = { go = true, rust = true, lua = true }
+      if enable_filetypes[vim.bo[bufnr].filetype] then
+        return {
+          timeout_ms = 500,
+          lsp_fallback = enable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end
+    end,
     formatters_by_ft = {
       -- lua = { 'stylua' },
       -- Conform will run multiple formatters sequentially

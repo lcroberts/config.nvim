07a632232a4df3eab079e119cd5d30680f9d6a21
Logan Roberts <logan@lcroberts.dev>
2025-03-29 11:49:34 -0400

    Update blink.cmp config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -43,22 +43,7 @@ return {
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
-
-      snippets = {
-        expand = function(snippet)
-          require('luasnip').lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction)
-          require('luasnip').jump(direction)
-        end,
-      },
-
+      snippets = { preset = 'luasnip' },
       signature = {
         enabled = true,
       },

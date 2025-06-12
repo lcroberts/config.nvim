5207cbb3b105e325878032b54fe1dbc1f55c8616
Logan Roberts <logan@lcroberts.dev>
2025-06-12 22:15:45 +0000

    Add check for gohtml

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,1 +1,13 @@
 vim.opt_local.shiftwidth = 2
+vim.cmd [[
+function DetectGoHtmlTmpl()
+    if expand('%:e') == "html" && search("{{") != 0
+        setfiletype gohtmltmpl
+    endif
+endfunction
+
+augroup filetypedetect
+    " gohtmltmpl
+    au BufRead,BufNewFile *.html call DetectGoHtmlTmpl()
+augroup END
+]]

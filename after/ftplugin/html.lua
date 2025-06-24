b74751556adc80a5165bb0e00a689e5a84d3b8da
Logan Roberts <logan@lcroberts.dev>
2025-06-23 21:28:32 -0400

    Fix thing

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,13 +1,1 @@
 vim.opt_local.shiftwidth = 2
-vim.cmd [[
-function DetectGoHtmlTmpl()
-    if expand('%:e') == "html" && search("{{") != 0
-        setfiletype gohtmltmpl
-    endif
-endfunction
-
-augroup filetypedetect
-    " gohtmltmpl
-    au BufRead,BufNewFile *.html call DetectGoHtmlTmpl()
-augroup END
-]]

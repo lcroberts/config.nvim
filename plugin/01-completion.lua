c3c2661bbf76f7637a589af188928079492dbe61
Logan Roberts <logan@lcroberts.dev>
2026-04-13 20:06:47 -0400

    Move around more stuffs

---
+++ plugin/01-completion.lua
@@ -0,0 +1,50 @@
+vim.pack.add({
+  {
+    src = 'https://github.com/saghen/blink.cmp',
+    version = "v1.*",
+  },
+  'https://github.com/rafamadriz/friendly-snippets',
+})
+
+require('blink-cmp').setup{
+  keymap = { preset = 'default' },
+  appearance = {
+    nerd_font_variant = 'mono',
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+  signature = {
+    enabled = true,
+  },
+  completion = {
+    documentation = {
+      auto_show = true,
+    },
+    menu = {
+      draw = {
+        padding = { 0, 1 },
+        columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              return ' ' .. ctx.kind_icon .. ' '
+            end,
+          },
+        },
+      },
+    },
+  },
+  sources = {
+    -- add lazydev to your completion providers
+    default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        -- make lazydev completions top priority (see `:h blink.cmp`)
+        score_offset = 100,
+      },
+    },
+  },
+}

99353dab76331229b1ab448af6fd5e5da534b9fa
Logan Roberts <logan@lcroberts.dev>
2026-04-14 20:48:13 -0400

    Get lazylangs sitch done

--- init.lua
+++ init.lua
@@ -44,23 +44,23 @@ vim.pack.clean = function()
   end
 end
 
--- local langs = { 'lua', 'bash', 'markdown' }
--- local success, file_langs = pcall(require, 'languages')
--- if success then
---   for _, language in ipairs(file_langs) do
---     if not vim.list_contains(langs, language) then
---       table.insert(langs, language)
---     end
---   end
--- end
--- vim.g.lazylangs = {
---   override_path = 'languages', -- '.' separated path relative to the lua directory
---   formatting_plugin = 'conform',
---   completion_plugin = 'blink.cmp',
---   linting_plugin = 'nvim-lint',
---   debugging_plugin = 'nvim-dap',
---   langs = langs, -- string[] of language names
--- }
+local langs = { 'lua', 'bash', 'markdown' }
+local success, file_langs = pcall(require, 'languages')
+if success then
+  for _, language in ipairs(file_langs) do
+    if not vim.list_contains(langs, language) then
+      table.insert(langs, language)
+    end
+  end
+end
+vim.g.lazylangs = {
+  override_path = 'languages', -- '.' separated path relative to the lua directory
+  formatting_plugin = 'conform',
+  completion_plugin = 'blink.cmp',
+  linting_plugin = 'nvim-lint',
+  debugging_plugin = 'nvim-dap',
+  langs = langs, -- string[] of language names
+}
 
 -- Create autocmds
 require 'autocmd'
---
+++ lua/helpers.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.plugin_path = vim.fs.abspath '~/Projects/nvim-plugins/'
+
+return M
